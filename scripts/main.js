3d4c39ff5e6e45fe83f73a0a218cd58733a2a5c4
youemomlol <7515701@isd535.org>
2021-09-16 09:43:02 -0500

    added assets

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
-// Put your JavaScript here
+let config = {
+    type: Meow.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Meow.Game(config);
+
+function preload() {
+    this.load.image('cat', 'assets/cat.png');
+    this.load.image('food', 'assets/cutefood.png');
+}
+
+function create() {
+    this.add.image(400, 300, 'cat');
+}
+
+function update() {
+}
