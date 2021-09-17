e86ae129b5b376d57a311e4f591ecf39101a8875
youemomlol <7515701@isd535.org>
2021-09-17 09:39:37 -0500

    added cat

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 let config = {
-    type: Meow.AUTO,
-    width: 800,
-    height: 600,
+    type: Phaser.AUTO,
+    width: 1600,
+    height: 750,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -9,15 +16,23 @@ let config = {
     }
 };
 
-let game = new Meow.Game(config);
+let game = new Phaser.Game(config);
+let platforms;
 
 function preload() {
     this.load.image('cat', 'assets/cat.png');
     this.load.image('food', 'assets/cutefood.png');
+    this.load.image('dog', 'assets/images.png');
 }
 
 function create() {
-    this.add.image(400, 300, 'cat');
+    this.add.image(800, 375, 'cat');
+
+    platforms = this.physics.add.staticgroup();
+
+    platforms.create(600, 400, 'dog');
+    platforms.create(50, 250, 'dog');
+    platforms.create(750, 220, 'dog');
 }
 
 function update() {
