3355145d45710a8bf28ea32bc84ad2c29480b6c5
youemomlol <7515701@isd535.org>
2021-09-20 09:40:50 -0500

    added collision between objects
    doesnt look good tho

--- scripts/main.js
+++ scripts/main.js
@@ -17,22 +17,40 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+let food;
 let platforms;
+let player;
 
 function preload() {
-    this.load.image('cat', 'assets/cat.png');
-    this.load.image('food', 'assets/cutefood.png');
-    this.load.image('dog', 'assets/images.png');
+    this.load.image('cat', 'assets/kitty.png');
+    this.load.image('food', 'assets/fish.png');
+    this.load.image('dog', 'assets/scarydog.png');
+    this.load.image('sky', 'assets/sky.jpg');
 }
 
 function create() {
-    this.add.image(800, 375, 'cat');
+    this.add.image(800, 375, 'sky');
 
-    platforms = this.physics.add.staticgroup();
+    
+    food = this.physics.add.staticGroup();
 
-    platforms.create(600, 400, 'dog');
-    platforms.create(50, 250, 'dog');
-    platforms.create(750, 220, 'dog');
+    food.create(800, 600, 'food').setScale(0.05);
+
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(800, 300, 'dog').setScale(0.2).refreshBody();
+
+
+    player = this.physics.add.image(800, 450, 'cat').setScale(0.5);
+
+    player.body.setSize(300, 250);
+    player.body.setOffset(10, 10);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, food);
 }
 
 function update() {
