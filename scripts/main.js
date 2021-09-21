fdc0df6e0cba5fa47652ba8ffe46f175ef463d3d
Jelle <7515701@isd535.org>
2021-09-21 09:42:59 -0500

    added veplocity to kitty

--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,18 @@ let config = {
     }
 };
 
+
 let game = new Phaser.Game(config);
 let food;
-let platforms;
+let pipes;
 let player;
+let cursors;
+
+let playerGravity = 1000;
+let pipesInterval = 2000;
+let pipesGroup;
+let playerFlapPower = 300;
+
 
 function preload() {
     this.load.image('cat', 'assets/kitty.png');
@@ -28,30 +36,42 @@ function preload() {
     this.load.image('sky', 'assets/sky.jpg');
 }
 
+
 function create() {
     this.add.image(800, 375, 'sky');
-
     
-    food = this.physics.add.staticGroup();
+   // food = this.physics.add.staticGroup();
+   // food.create(800, 600, 'food').setScale(0.05);
 
-    food.create(800, 600, 'food').setScale(0.05);
+    pipes = this.physics.add.staticGroup();
 
+    pipes.create(800, 300, 'dog').setScale(0.2).refreshBody();
 
-    platforms = this.physics.add.staticGroup();
-
-    platforms.create(800, 300, 'dog').setScale(0.2).refreshBody();
-
-
+    //add kitty
     player = this.physics.add.image(800, 450, 'cat').setScale(0.5);
-
+    //kitty physics
     player.body.setSize(300, 250);
-    player.body.setOffset(10, 10);
+    player.body.setOffset(100, 100);
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+    player.body.gravity.y = playerGravity;
+    player.body.velocity.y = -playerFlapPower;
 
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(player, food);
+    game.time.events.loop(pipesInterval, addPipes); 
+			addPipes();
+
+    this.physics.add.collider(player, pipes);
+    // this.physics.add.collider(player, food);
+
+    cursors = this.input.keyboard.createCursorKeys();
+   
 }
 
+
+
 function update() {
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-350);
+    }
 }
